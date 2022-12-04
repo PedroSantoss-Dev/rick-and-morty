eab7afba62b8680eb757cf2187d3f3f938a780a4
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2022-12-04 19:36:18 -0300

    Update index.js

--- src/shared/components/Slider/index.js
+++ src/shared/components/Slider/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { Zoom } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+import Api from '../../service/api';
 import * as S from "./style"
 
 
 const Slideshow = () => {
-  const images = [
-    {
-      url: 'https://img.estadao.com.br/thumbs/640/resources/jpg/5/0/1557958505405.jpg',
-    },
-    {
-      url: 'https://imagensemoldes.com.br/wp-content/uploads/2021/04/Logo-Rick-and-Morty-PNG.png',
-    },
-    {
-      url: 'https://www.sofilmesgratis.com/wp-content/uploads/2021/07/Gravity-Falls.jpg',
-    },
-  ];
+ const [ images , setImages ] = useState([])
+
+   const data = async () => {
+   await Api.get('character/1,183,16')  
+   .then(results => {
+    setImages(results.data)
+   }).catch((err) => console.log(err))
+  }
+
+  
+  useEffect(() => {
+    data()
+  })
+
 
   const zoomOutProperties = {
     indicators: true,
@@ -27,7 +32,7 @@ const Slideshow = () => {
       <Zoom className='slide_zoon' {...zoomOutProperties}>
         { images.map((each, index) => (
             <>
-            <S.Img key={index}  src={each.url} />
+            <S.Img key={index}  src={each.image} />
             <S.Name>{each.nome}</S.Name>
             </>
         ))}
